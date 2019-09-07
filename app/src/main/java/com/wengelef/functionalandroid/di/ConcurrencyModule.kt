d0530f9f8bc64c6f50b5460b76eade1990a82341
Florian Wengelewski <wengelef@gmail.com>
2019-09-07 23:11:31 +0200

    fix : named dependencies

--- app/src/main/java/com/wengelef/functionalandroid/di/ConcurrencyModule.kt
+++ app/src/main/java/com/wengelef/functionalandroid/di/ConcurrencyModule.kt
@@ -3,10 +3,11 @@ package com.wengelef.functionalandroid.di
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import util.Dispatcher
-import util.IO
 
 object ConcurrencyModule {
     operator fun invoke() = module {
-        single<Dispatcher>(named("IO")) { IO }
+        single<Dispatcher>(named(IO)) { util.IO }
     }
+
+    const val IO = "IO"
 }
