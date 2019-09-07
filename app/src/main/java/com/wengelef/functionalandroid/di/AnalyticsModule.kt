9fa227e2c5a30c1505eb2adc058ef36de964b0aa
Florian Wengelewski <wengelef@gmail.com>
2019-09-07 16:45:06 +0200

    fix : LoginViewModel DI, wrong type for Tracker

--- app/src/main/java/com/wengelef/functionalandroid/di/AnalyticsModule.kt
+++ app/src/main/java/com/wengelef/functionalandroid/di/AnalyticsModule.kt
@@ -3,12 +3,13 @@ package com.wengelef.functionalandroid.di
 import analyticslibrary.analyticsTracker
 import analyticslibrary.loginEventMapper
 import org.koin.dsl.module
+import tracking.LoginTracker
 import tracking.getTracker
 
 object AnalyticsModule {
     operator fun invoke() = module {
         single {
-            getTracker(::analyticsTracker, ::loginEventMapper)
+            LoginTracker(getTracker(::analyticsTracker, ::loginEventMapper))
         }
     }
 }
